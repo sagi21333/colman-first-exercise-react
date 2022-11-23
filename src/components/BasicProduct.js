576f4392bbd4e17ccfb0c388886a53a0eede1881
Sagi Bwn Mordechai <sagi21333@gmail.com>
2022-11-23 17:23:12 +0200

    css to basic product

--- src/components/BasicProduct.js
+++ src/components/BasicProduct.js
@@ -4,9 +4,9 @@ import './BasicProduct.css';
 const BasicProduct = (props) => {
 
     return(
-        <div >
-            <h2 >{props.title} </h2>
-            <div >{props.price} </div>
+        <div className="basic-product">
+            <h2 className="basic-product__title">{props.title} </h2>
+            <div className="basic-product__price">{props.price} </div>
             <img src={props.imageUrl}  />
         </div>
     );
